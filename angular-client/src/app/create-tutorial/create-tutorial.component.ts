312e925171bd758735799015d557e8f4e6c205c8
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-09-08 21:23:48 +0100

    Made 0 progress.. Need reactive forms!

--- angular-client/src/app/create-tutorial/create-tutorial.component.ts
+++ angular-client/src/app/create-tutorial/create-tutorial.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from "@angular/core";
-
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+/*
+  {
+    "tutorialName": "Name",
+      "sections": [
+        {
+          "sectionName": "Section name",
+          "sectionDetails": [
+            {
+              "detailHeader": "Header name",
+              "detailImages": ["base 64"],
+              "detailText": "TITS"
+            }
+          ]
+        },
+      ]
+  }
+*/
 @Component({
   selector: "create-tutorial",
   templateUrl: "./create-tutorial.component.html",
   styleUrls: ["./create-tutorial.component.scss"]
 })
-export class CreateTutorialComponent implements OnInit {
-
-  // TODO: Make section data model (interface)
-  private sections: Array<any> = [];
+export class CreateTutorialComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
-  public generateSection(): void {
-    this.sections.push(1);
-  }
 
 }
--- angular-client/src/app/create-tutorial/create-tutorial.module.ts
+++ angular-client/src/app/create-tutorial/create-tutorial.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CreateTutorialComponent } from "./create-tutorial.component";
 import { CreateTutorialRoutingModule } from "./create-tutorial-routing.module";
-import { CreateTutorialSectionComponent } from "./create-tutorial-section/create-tutorial-section.component";
 import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [
-    CreateTutorialComponent,
-    CreateTutorialSectionComponent
+    CreateTutorialComponent
   ],
   imports: [
     CommonModule,
