40707519119a139e15b4cf44691de146fca3819b
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-09-09 11:54:07 +0100

    Add reactive form module & starting of reactive tutorial form

--- angular-client/src/app/create-tutorial/create-tutorial.component.ts
+++ angular-client/src/app/create-tutorial/create-tutorial.component.ts
@@ -1,4 +1,49 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, FormArray, Form } from "@angular/forms";
+@Component({
+  selector: "create-tutorial",
+  templateUrl: "./create-tutorial.component.html",
+  styleUrls: ["./create-tutorial.component.scss"]
+})
+export class CreateTutorialComponent implements OnInit {
+
+  public form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      tutorialName: "",
+      sections: this.formBuilder.array([])
+    });
+  }
+
+  public get sectionForms(): FormArray {
+    return this.form.get("sections") as FormArray;
+  }
+
+  public addSectionForm(): void {
+    const section = this.formBuilder.group({
+      sectionName: ""
+    });
+
+    this.sectionForms.push(section);
+  }
+
+  public removeSectionForm(sectionIndex): void {
+    this.sectionForms.removeAt(sectionIndex);
+
+  }
+
+}
+
+
+
+
+
+
 /*
   {
     "tutorialName": "Name",
@@ -16,13 +61,6 @@ import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
       ]
   }
 */
-@Component({
-  selector: "create-tutorial",
-  templateUrl: "./create-tutorial.component.html",
-  styleUrls: ["./create-tutorial.component.scss"]
-})
-export class CreateTutorialComponent {
 
-  constructor() { }
-
-}
+// get data from form
+// this.form.valueChanges.subscribe(console.log);
--- angular-client/src/app/create-tutorial/create-tutorial.module.ts
+++ angular-client/src/app/create-tutorial/create-tutorial.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { CreateTutorialComponent } from "./create-tutorial.component";
 import { CreateTutorialRoutingModule } from "./create-tutorial-routing.module";
 import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { CommonModule } from "@angular/common";
   ],
   imports: [
     CommonModule,
-    CreateTutorialRoutingModule
+    CreateTutorialRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class CreateTutorialModule { }
